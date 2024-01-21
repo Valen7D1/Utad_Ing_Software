234d292d1b6d209ab4b202d6168f9ff282e865f0
Gonzalo Valenti <78683787+ValenUC3M@users.noreply.github.com>
2024-01-21 18:00:08 +0100

    Separated Timer from gameLogic

---
+++ Skeleton/1_Skeleton/1_Skeleton/swalib-master/common/logicSlot.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <Windows.h>
+
+class Timer
+{
+public:
+    Timer();
+
+    //setters
+    void SetTimer();
+
+    //getters
+    float GetFrameTime();
+    double GetTotalTime();
+    double GetTime_fps();
+    double GetTotalFrameTime();
+
+    void InitTimer(); // get starting values for frequency and actual time
+    bool ProcessSlots(); // checker for logic loop
+    void InitSlotsToProcess(); // calc all the new values
+
+private:
+    LARGE_INTEGER frequency;
+    LARGE_INTEGER actualTime;
+    LARGE_INTEGER previousTime;
+
+    // time control/render values
+    double elapsedTime;
+    double totalTime;
+    double totalElapsed;
+    double time_fps;
+    double totalFrameTime;
+
+    float frameTime = 1.0f / 60.0f; // Target time per frame for 60 fps
+};
+
