fb7af8781889ef366dea06978a14ddd586cc4ad7
Gonzalo Valenti <78683787+ValenUC3M@users.noreply.github.com>
2024-02-24 16:37:47 +0100

    added comments

--- Final_SP/common/ladderRenderComponent.cpp
+++ Final_SP/common/ladderRenderComponent.cpp
@@ -5,9 +5,11 @@
 
 void LadderRenderComponent::Slot()
 {
+	// simplified version of render for ladders
 	CORE_RenderCenteredSprite(m_position, m_radius, m_sprite);
 }
 
+// ladders dont really comunicate do them? xd
 void LadderRenderComponent::ReceiveMessage(Message* msg)
 {
 }
--- Final_SP/common/platformRenderComponent.cpp
+++ Final_SP/common/platformRenderComponent.cpp
@@ -5,12 +5,13 @@
 
 void PLatformRenderComponent::Slot()
 {
+	// define central position, get proportions and size
 	float sizeX = m_size.x / m_proportions.x;
 	float sizeY = m_size.y / m_proportions.y;
 
 	vec2 initialPosition = vec2(m_position-m_size/2);
 
-	//platforms render
+	// render as many platforms squares as needed
 	vec2 position;
 	position.x = initialPosition.x + m_proportions.x / 2;
 	position.y = initialPosition.y + m_proportions.y / 2;
@@ -25,6 +26,7 @@ void PLatformRenderComponent::Slot()
 	}
 }
 
+// platforms dont really comunicate do them? xd
 void PLatformRenderComponent::ReceiveMessage(Message* msg)
 {
 }
